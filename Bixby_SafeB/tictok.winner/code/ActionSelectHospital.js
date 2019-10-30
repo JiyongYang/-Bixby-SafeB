5fec7de312d6a6418f97b8feafadf38d69ffd550
Ed <elleinuo0@gmail.com>
2019-10-30 14:48:53 +0900

    modify files

--- Bixby_SafeB/tictok.winner/code/ActionSelectHospital.js
+++
@@ -1,9 +0,0 @@
-module.exports.function = function actionSelectHospital (results, order) {
-  let hospital;
-  if(order == "first"){
-    hospital = results.hospital[0];
-  }else if(order=="second"){
-    hospital = results.hospital[1];
-  }
-  return hospital;
-}
--- Bixby_SafeB/tictok.winner/code/DirectGetHospitalGradeInfo.js
+++
@@ -1,4 +0,0 @@
-module.exports.function = function filename () {
-  let infos = require('./data/GradeInfo.js');
-  return infos;
-}
